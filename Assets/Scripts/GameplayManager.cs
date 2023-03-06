d994bfc96615b89531dfdd845b443538f7119ff6
gnendraaw <itsgnendraaw@gmail.com>
2023-03-06 21:25:41 +0800

    add new player controller & gameplay manager script

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public static bool isGameOver {get; private set;}
+    public static bool isGamePaused {get; private set;}
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float deltaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !GameplayManager.isGameOver)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        Touch touch = Input.GetTouch(0);
+        Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            deltaX = touchPos.x - transform.position.x;
+        }
+        if (touch.phase == TouchPhase.Moved) 
+        {
+            rb.MovePosition(Vector2.right * (touchPos.x - deltaX));
+        }
+        if (touch.phase == TouchPhase.Ended)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
